ad68f9c0627c7bb40b65587238ead33442634d77
jdlrobson <jdlrobson@gmail.com>
2019-02-09 17:19:47 -0800

    Slideshow shows indications of size

--- public/index.js
+++ public/index.js
@@ -1,4 +1,19 @@
+var COIN_CLASS = 'slideshow__counter__coin';
+var ACTIVE_COIN_CLASS = COIN_CLASS + ' slideshow__counter__coin--active'
+
 function initSlideshow(slideshow) {
+    var coin, activeCoin = 0, coins;
+    var num = slideshow.querySelectorAll('.slideshow__item').length;
+    var indicators = document.createElement( 'ul' );
+    indicators.className = 'slideshow__counter';
+    for ( var i = 0; i < num; i++ ) {
+        coin = document.createElement( 'li' );
+        coin.className = COIN_CLASS;
+        indicators.appendChild( coin );
+    }
+    coins = indicators.childNodes;
+    coins[activeCoin].className = ACTIVE_COIN_CLASS;
+    slideshow.insertBefore( indicators, slideshow.firstChild );
     Array.from(slideshow.querySelectorAll('.slideshow__button')).forEach(function(item) {
         item.addEventListener('click', function (ev) {
             var activeClass = 'slideshow__item slideshow__item--active';
@@ -8,9 +23,15 @@ function initSlideshow(slideshow) {
             var next = dirRight ? active.nextSibling : active.previousSibling;
             // Move to next
             active.className = 'slideshow__item';
+            // update coin
+            coins[activeCoin].className = COIN_CLASS;
+            activeCoin += dirRight ? 1 : -1;
             if ( !next ) {
+                activeCoin = dirRight ? 0 : coins.length - 1;
                 next = dirRight ? active.parentNode.firstChild : active.parentNode.lastChild;
             }
+            // update new coin
+            coins[activeCoin].className = ACTIVE_COIN_CLASS;
             next.className = activeClass;
         } );
     } );
