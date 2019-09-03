064f9efea5c3eacba8476a993bedcfde64f9b373
jdlrobson <jdlrobson@gmail.com>
2019-09-02 18:29:55 -0700

    An actual RSS feed

--- functions/rss.js
+++ functions/rss.js
@@ -1,15 +1,34 @@
+const RSS = require('rss');
 const fetch = require('node-fetch');
 const xml2js = require('xml2js-es6-promise');
-
 exports.handler = function (_event, _context, callback) {
+
+    const feed = new RSS({
+        title: 'Jon Robson',
+        description: 'Things about Jon'
+    });
     fetch( 'https://medium.com/feed/@dlyall' ).then((response) => response.text())
         .then((xml)=> {
             return xml2js(xml);
         })
         .then((data) => {
+            data.rss.channel[0].item.forEach((item) => {
+                if ( item.category) {
+                    console.log(item.title[0]);
+                    feed.item( {
+                        title: item.title[0],
+                        description: item.description || item['content:encoded'][0],
+                        url: item.link[0],
+                        date: item.pubDate[0]
+                    })
+                }
+            });
             callback(null, {
+                headers: {
+                    'content-type': 'text/xml'
+                },
                 statusCode: 200,
-                body: JSON.stringify(data.rss.channel[0].item)
+                body: feed.xml({indent: true})
               })
         });
 }
