bdcc57994c263ca4e4a4a8d12e696bb35fb1e244
jdlrobson <jdlrobson@gmail.com>
2019-09-03 22:02:20 -0700

    Add instagram to my feed

--- functions/rss.js
+++ functions/rss.js
@@ -1,20 +1,53 @@
 const RSS = require('rss');
 const fetch = require('node-fetch');
 const xml2js = require('xml2js-es6-promise');
+
 exports.handler = function (_event, _context, callback) {
 
     const feed = new RSS({
         title: 'Jon Robson',
         description: 'Things about Jon'
     });
-    fetch( 'https://medium.com/feed/@dlyall' ).then((response) => response.text())
-        .then((xml)=> {
-            return xml2js(xml);
-        })
-        .then((data) => {
-            data.rss.channel[0].item.forEach((item) => {
+    Promise.all(
+        [
+            fetch( 'https://medium.com/feed/@dlyall' ).then((response) => response.text())
+                .then((xml) => xml2js(xml)),
+            fetch( 'https://www.instagram.com/jdlrobson/?__a=1' ).then(response=>response.json())
+        ]
+    )
+        .then(([ mediumdata, igdata ] )=> {
+            let igramposts;
+            try {
+                igramposts = igdata.graphql.user.edge_owner_to_timeline_media.edges;
+            } catch (e) {
+                igramposts = [];
+            }
+
+            // extract from igram
+            igramposts.forEach((post) => {
+                let title;
+                const node = post.node;
+                try {
+                    title = node.edge_media_to_caption.edges[0].node.text
+                        .match(/([A-Z: \\n]+)/)[0];
+                    if ( title.length > 1 ) {
+                        feed.item( {
+                            title,
+                            enclosure: {
+                                url: node.thumbnail_src,
+                                type: 'image/jpeg'
+                            },
+                            url: `https://www.instagram.com/p/${post.shortcode}/`,
+                            date: new Date( node.taken_at_timestamp * 1000 )
+                        });
+                    }
+                } catch (e) {
+                    // no title.
+                }
+            })
+            // extract from medium
+            mediumdata.rss.channel[0].item.forEach((item) => {
                 if ( item.category) {
-                    console.log(item.title[0]);
                     feed.item( {
                         title: item.title[0],
                         description: item.description || item['content:encoded'][0],
