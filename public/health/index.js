b6c74e12f24136e32506197f1d5eabacb6ceadb2
Jon Robson <jdlrobson@gmail.com>
2025-11-02 16:26:26 -0800

    Drop food tracking, add more exercise options

--- public/health/index.js
+++ public/health/index.js
@@ -68,22 +68,6 @@ const save = () => {
     refresh();
 }
 
-const scoreFood = ( stats ) => {
-    // X = Y
-    // X = 100 / 21 = 5
-    // Z = 50 / 21 = 2
-    const hasMehFoods = !!stats.mehFoods;
-    const mehFoods = hasMehFoods ? stats.mehFoods.progress : 0;
-    const goodFoods = stats.goodFoods.progress;
-    const badFoods = stats.badFoods.progress;
-    const missingMeals = 21 - ( mehFoods + badFoods + goodFoods );
-    const standardMeals = missingMeals + mehFoods;
-
-    const foodScore = ( 4.8 * goodFoods ) - ( 4.8 * badFoods ) + ( 2.5 * standardMeals );
-    console.log(foodScore);
-    return (new Array( Math.round( foodScore / 15 ) )).fill(`⭐`).join('');
-};
-
 const previousWeeks = (() => {
     let pastWeek = weekNo;
     const history = [];
@@ -95,8 +79,7 @@ const previousWeeks = (() => {
         const previous = localStorage.getItem(`week-stats-${pastWeek}`);
         if ( previous ) {
             const stats = JSON.parse(previous);
-            const foodStars = scoreFood(stats);
-            history.push(`[week ${pastWeek}] Exercise: ${calc(stats.exercise)}% | Food score ${foodStars}`);
+            history.push(`[week ${pastWeek}] Exercise: ${calc(stats.exercise)}%`);
         } else {
             history.push(`[week ${pastWeek}] n/a`);
         }
@@ -105,41 +88,19 @@ const previousWeeks = (() => {
 })();
 
 const refresh = () => {
-    const hasFoodStat = stats.goodFoods.progress || stats.mehFoods.progress || stats.badFoods.progress;
     app.innerHTML = `
     <h1>Progress week ${ weekNo } / 52</h1>
     <p>Week for ${ lastSunday.getDate() } / ${ lastSunday.getMonth() + 1 } / ${ lastSunday.getFullYear() }
     <h2>Exercise</h2>
     <progress-bar>${stats.exercise.progress / stats.exercise.target}</progress-bar>
     <strong>${stats.exercise.progress}</strong>/<strong>${stats.exercise.target}</strong>
-   <button data-key="exercise" data-increment="5">+5m</button>
-  <button data-key="exercise" data-increment="15">+15m</button>
-      <button data-key="exercise" data-increment="30">+30m</button>
+    <button data-key="exercise" data-increment="5">+5m</button>
+    <button data-key="exercise" data-increment="10">+10m</button>
+    <button data-key="exercise" data-increment="15">+15m</button>
+    <button data-key="exercise" data-increment="20">+20m</button>
+    <button data-key="exercise" data-increment="30">+30m</button>
     <button data-key="exercise" data-increment="45">+45m</button>
     <button data-key="exercise" data-increment="60">+60m</button>
-    <h2>Meals</h2>
-    <span>Current: ${ hasFoodStat ? scoreFood( stats ) : '_' } </span>
-    <progress-bar>${ stats.goodFoods.progress / stats.goodFoods.target }</progress-bar>
-    <strong>🙂${stats.goodFoods.progress}</strong>/<strong>${stats.goodFoods.target}</strong>
-    <progress-bar>${ stats.mehFoods.progress / stats.mehFoods.target }</progress-bar>
-    <strong>🤷‍♂️${stats.mehFoods.progress}</strong>/<strong>${stats.mehFoods.target}</strong>
-    <progress-bar>${ stats.badFoods.progress / stats.badFoods.target }</progress-bar>
-    <strong>☹️${stats.badFoods.progress}</strong>/<strong>${stats.badFoods.target}</strong>
-    <div>
-    <button data-key="badFoods" data-increment="1">☹️</button>
-    <button data-key="mehFoods" data-increment="1">🤷‍♂️</button>
-    <button data-key="goodFoods" data-increment="1">🙂</button>
-    </div>
-    <h3>Food notes</h3>
-    <table>
-    <tr><th>Good</th><th>Bad</th></tr>
-    <tr>
-    <td>beans</td><td>trans fats</td></tr>
-    <tr><td>barley</li></td><td>red meat</td></tr>
-    <tr><td>fish</li></td><td>fried food</td></tr>
-    <tr><td>nuts</li></td><td></td></tr>
-    <tr><td>bananas</li></td><td></td></tr>
-    </table>
     <h2>previously</h2>
     <ul class="listBlock listItalic">${previousWeeks.length ? previousWeeks.map((l)=>`<li>${l}</li>`).join('') : 'N/A'}</ul>
     <h2>log</h2>
